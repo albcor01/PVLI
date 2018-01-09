a3182a46064ccd056aef7b751bdbec8a7b496aff
Alberto <albcor01@ucm.es>
2018-01-09 20:32:27 +0100

    cambios entre ramas

--- plantilla-juego/src/js/play_scene.js
+++ plantilla-juego/src/js/play_scene.js
@@ -18,23 +18,61 @@ create: function() {
   this.congelado=false;
   this.map = this.game.add.tilemap('level1');
   this.map.addTilesetImage('MicroMachines2-GG-TreehouseTiles');
-  this.layer = this.map.createLayer('Floor');
+  this.layer = this.map.createLayer('Capa de Patrones 1');
   this.layer.resizeWorld();
   this.Numbalas=30;
-//creamos obstaculos
-  //Agujero
-  this.agujero=new GO.gameObject(this.game,'agujero',this.levelData.layers[2].objects[0].x + 500, this.levelData.layers[2].objects[0].y+500,0.5,0.5,0.5,0.5);
-  this.agujero.sprite.body.setSize(400,400,50,50);
-  //aceite
-  this.aceite=new GO.gameObject(this.game,'aceite',this.levelData.layers[2].objects[0].x + 600, this.levelData.layers[2].objects[0].y,0.5,0.5,0.25,0.5);
-  this.aceite.sprite.body.setSize(1000,300,-200,200);
-  //charco
-  this.charco = new GO.gameObject(this.game, 'charco', this.levelData.layers[2].objects[0].x + 700, this.levelData.layers[2].objects[0].y-500, 0, 0, 0.15, 0.6);
-  this.charco.sprite.body.setSize(420, 170, 200, -170);
- //Pongo una banderita para hacer una prueba de movimiento (ESTO SE QUITARÁ)
-  this.puntos = this.levelData.layers[1].objects.length;
-  this.banderas = [];
 
+//creamos obstaculos
+  /****************************************************************************/
+  //Creamos un array de objetos que son obstaculos como elementos colisionables, agujeros
+  //o charcos que relentizan o resbalan 
+  this.numHoles = this.levelData.layers[3].objects.length;
+  this.numCharcos = this.levelData.layers[5].objects.length;
+  this.numResbala = this.levelData.layers[7].objects.length;
+  this.charcos = [];
+  this.holes = [];
+  this.resbala = [];
+
+  for(var i = 0; i < this.numHoles; i++)
+  {
+    this.holes.push(this.game.add.sprite(this.levelData.layers[3].objects[i].x, this.levelData.layers[3].objects[i].y));
+    this.game.physics.enable(this.holes[i],Phaser.Physics.ARCADE);
+    this.holes[i].body.setSize(this.levelData.layers[3].objects[i].width, this.levelData.layers[3].objects[i].height, 0, 0);
+  }
+
+  for(var i = 0; i < this.numCharcos; i++)
+  {
+    this.charcos.push(this.game.add.sprite(this.levelData.layers[5].objects[i].x, this.levelData.layers[5].objects[i].y));
+    this.game.physics.enable(this.charcos[i],Phaser.Physics.ARCADE);
+    this.charcos[i].body.setSize(this.levelData.layers[5].objects[i].width, this.levelData.layers[5].objects[i].height, 0, 0);
+  }
+
+  for(var i = 0; i < this.numResbala; i++)
+  {
+    this.resbala.push(this.game.add.sprite(this.levelData.layers[7].objects[i].x, this.levelData.layers[7].objects[i].y));
+    this.game.physics.enable(this.resbala[i],Phaser.Physics.ARCADE);
+    this.resbala[i].body.setSize(this.levelData.layers[7].objects[i].width, this.levelData.layers[7].objects[i].height, 0, 0);
+  }
+
+  //PARA RECORDAR COMO LO HACIA ANTES
+  /*this.aceite=new GO.gameObject(this.game,'aceite',this.levelData.layers[2].objects[0].x + 600, this.levelData.layers[2].objects[0].y,0.5,0.5,0.25,0.5);
+  this.aceite.sprite.body.setSize(1000,300,-200,200);*/
+
+  //AQUÍ TERMINA LA CREACION DE OBSTACULOS
+/******************************************************************************/
+
+
+/****************************************************************************/
+ //Pongo una banderita para hacer una prueba de movimiento (ESTO SE QUITARÁ)
+ //CREAMOS LOS ARRAYS QUE CONTENDRÁN EN ORDEN LAS BANDERAS 
+ //LOS FOR POSTERIORES INTRODUCEN EN EL ARRAY LA POSICION DE LAS BANDERA
+ //LEYENDO EL JSON DEL MAPA
+  this.puntos = this.levelData.layers[1].objects.length;
+  this.puntos2 = this.levelData.layers[2].objects.length;
+  this.puntos3 = this.levelData.layers[6].objects.length;
+  this.banderas = [];
+  this.banderas2 = [];
+  this.banderas3 = [];
   
   for(var i = 0; i < this.puntos; i++)
   {
@@ -42,9 +80,24 @@ create: function() {
     this.game.physics.enable(this.banderas[i],Phaser.Physics.ARCADE);
     this.banderas[i].body.setSize(100, 100, -50, -50);
   }
-  //console.log(this.banderas.length);
 
- 
+  for(var i = 0; i < this.puntos2; i++)
+  {
+    this.banderas2.push(this.game.add.sprite(this.levelData.layers[2].objects[i].x, this.levelData.layers[2].objects[i].y));
+    this.game.physics.enable(this.banderas2[i],Phaser.Physics.ARCADE);
+    this.banderas2[i].body.setSize(100, 100, -50, -50);
+  }
+
+  for(var i = 0; i < this.puntos3; i++)
+  {
+    this.banderas3.push(this.game.add.sprite(this.levelData.layers[6].objects[i].x, this.levelData.layers[6].objects[i].y));
+    this.game.physics.enable(this.banderas3[i],Phaser.Physics.ARCADE);
+    this.banderas3[i].body.setSize(100, 100, -50, -50);
+  }
+  //console.log(this.banderas.length);
+  //AQUÍ TERMINA LA INTRODUCCION DE LAS BANDERAS
+  //EN SUS RESPECTIVOS ARRAYS
+ /****************************************************************************/
   //creamos al personajes
   this.jugador = new GO.player(this.game, 'car', this.levelData.layers[2].objects[0].x, this.levelData.layers[2].objects[0].y, 0.5, 0.5, 0.5, 0.5);
   this.enemy = new GO.enemigo(this.game, 2, 'carEnemy', this.levelData.layers[2].objects[1].x, this.levelData.layers[2].objects[1].y, 0.5, 0.5, 0.5, 0.5);
@@ -71,14 +124,20 @@ update: function() {
    this.enemy.update(this.game, this.banderas);
   
   //UPDATE DE DETECCIÓN DE ELEMENTOS DEL MAPA
-   this.jugador.detectaCharco(this.game, this.charco.sprite);
-   this.enemy.detectaCharco(this.game, this.charco.sprite);
-   this.jugador.muerte(this.game,this.agujero.sprite, this.levelData.layers[2].objects[0].x, this.levelData.layers[2].objects[0].y);
-   this.enemy.muerte(this.game,this.agujero.sprite, this.levelData.layers[2].objects[0].x, this.levelData.layers[2].objects[0].y);
-   this.enemy.congelado(this.weapon,this.congelado)
-   console.log(this.congelado);
-   this.jugador.Patinar(this.game,this.aceite.sprite);
+  //charco
+   //this.jugador.detectaCharco(this.game, this.charco.sprite);
+   //this.enemy.detectaCharco(this.game, this.charco.sprite);
+  //agujero
+   //this.jugador.muerte(this.game, this.agujero.sprite, this.levelData.layers[2].objects[0].x, this.levelData.layers[2].objects[0].y);
+   //this.enemy.muerte(this.game, this.agujero.sprite, this.levelData.layers[2].objects[0].x, this.levelData.layers[2].objects[0].y);
+  //enemigo congelado
+   //this.enemy.congelado(this.weapon,this.congelado)
 
+   //console.log(this.congelado);
+  //jugador pisa resbala
+   //this.jugador.Patinar(this.game,this.aceite.sprite);
+   
+  //ESTA PARTE DEL CÓDIGO DEFINE A QUIEN SIGUE LA CÁMARA EN FUNCIÓN DE SI EL JUGADOR HA CAIDO EN UN AGUJERO O NO
    if(this.jugador.sprite.alive)
    this.game.camera.follow(this.jugador.sprite, Phaser.Camera.FOLLOW_LOCKON, 0.8, 0.8);
    else this.game.camera.follow(this.enemy.sprite, Phaser.Camera.FOLLOW_LOCKON, 0.8, 0.8);
